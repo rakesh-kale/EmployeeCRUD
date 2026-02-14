ac8701003d93c5150af31b31398cdf15e298c000
Rakes <Rakes@RAKESH>
2026-02-14 21:56:34 +0530

    Add project files.

---
+++ WEBAPICRUD/Controllers/StudentAPIController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WEBAPICRUD.Models;
+
+namespace WEBAPICRUD.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentAPIController : ControllerBase
+    {
+
+        private readonly MyDbContext context;
+
+        public StudentAPIController(MyDbContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Student>>> GetStudents()
+        {
+            var data = await context.Students.ToListAsync();
+            return Ok(data);
+        }
+
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Student>> GetStudentById(int id)
+        {
+            var student = await context.Students.FindAsync(id);
+
+            if (student == null)
+            {
+                return NotFound();
+            }
+
+            return student;
+        }
+
+
+        [HttpPost]
+        public async Task<ActionResult<Student>> CreateStudent(int id, Student std)
+        {
+            await context.Students.AddAsync(std);
+            await context.SaveChangesAsync();
+            return Ok(std);
+        }
+
+    }
+}
---
+++ WEBAPICRUD/Models/Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WEBAPICRUD.Models;
+
+public partial class Student
+{
+    public string? Students { get; set; }
+
+    public string? StudentsName { get; set; }
+
+    public string? StudentsGender { get; set; }
+
+    public byte? Age { get; set; }
+
+    public string? Standard { get; set; }
+
+    public string? Fathername { get; set; }
+
+    public int Id { get; set; }
+}
